8907a46e0d984e3d78dda2a187171d015c416d42
asna sharin <asnasherin1233@gmail.com>
2025-03-04 00:53:07 +0530

    fix

--- frontend/src/AxiosConfig/axiosInstance.ts
+++ frontend/src/AxiosConfig/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: "https://doctimebackendd.onrender.com"
+  baseURL: "https://doctimebackendd.onrender.com/auth"
 });
 
 export default axiosInstance;
