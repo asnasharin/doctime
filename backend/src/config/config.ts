d8234e2996a4c520639c954e4ec44a3ca93ef6f2
asna sharin <asnasherin1233@gmail.com>
2025-03-03 19:11:37 +0530

    fix

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,7 +3,7 @@ dotenv.config()
 export default {
   port: process.env.PORT || 3000,
   mongo: {
-    uri: 'mongodb+srv://asnasherin1233:asna2926@cluster0.wdyqeu9.mongodb.net/doctime?retryWrites=true&w=majority&appName=Cluster0',
+    uri: process.env.MONGO_URL,
   },
 };
      
